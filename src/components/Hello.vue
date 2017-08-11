d8b3b5e4ac43dccac5278860843fb69be0ca984e
Shaun Savage <savages@savages.com>
2017-08-11 23:33:40 +0000

    initial

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <rich></rich>
+    <div v-for="qline in poll">
+      <qline :qline="qline"></qline>
+    </div>
+  </div>
+</template>
+
+<script>
+import Rich1 from './Rich1'
+import QLine from './QLine'
+
+export default {
+  name: 'hello',
+  components: {
+    'rich': Rich1,
+    'qline': QLine
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      'poll': [
+        {line: 'line0', type: 'check'},
+        {line: 'line1', type: 'radio'},
+        {line: 'line2', type: 'check'}
+      ],
+      line1Msg: 'line one'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/QLine.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="qline">
+    <div v-if="checkbox">
+      <input type="checkbox" id="cb1" />
+    </div>
+    <div v-if="radio">
+      <input type="radio" id="rd1" />
+    </div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QLine',
+  props: {
+    qline: {
+    }
+  },
+  data () {
+    return {
+      checkbox: false,
+      radio: false,
+      msg: ''
+    }
+  },
+  created () {
+    this.msg = this.qline.line
+    this.checkbox = this.qline.type === 'check'
+    this.radio = this.qline.type === 'radio'
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
